eb82188e6d1b0dd0468fd6a6a4407045054dbfe9
Cas154 <peterscas48@gmail.com>
2022-04-26 14:43:48 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, Link, useParams } from "react-router-dom";
+import { Routes, Route, useParams, Link } from "react-router-dom";
 import PokemonList from "./PokemonList";
 import axios from "axios";
 import "./pokeball.css";
 import Pagination from "./Pagination";
+import "./app.css";
 
 function Home() {
   const [pokemon, setPokemon] = useState([]);
   const [currentPageUrl, setCurrentPageUrl] = useState(
-    "https://pokeapi.co/api/v2/pokemon"
+    "https://pokeapi.co/api/v2/pokemon/"
   );
   const [nextPageUrl, setnextPageUrl] = useState();
   const [prevPageUrl, setPrevPageUrl] = useState();
@@ -34,8 +35,8 @@ function Home() {
 
   if (loading) {
     return (
-      <div class="container">
-        <div class="pokeball"></div>
+      <div className="container">
+        <div className="pokeball"></div>
       </div>
     );
   }
@@ -53,23 +54,59 @@ function Home() {
 
 function Pokemon() {
   const { name } = useParams();
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState();
+  const [pokemonTypes, setPokemonTypes] = useState();
+  const [pokemonData, setPokemonData] = useState([]);
+  const toArray = [];
   const [currentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+
   console.log(name);
 
   useEffect(() => {
     axios.get(currentPageUrl + name).then((res) => {
       console.log(res.data);
-
+      toArray.push(res.data);
+      setPokemonTypes(res.data.types.map((type) => type.type.name + "/"));
+      setPokemonData(toArray);
       setPokemon(res.data.results);
     });
   }, [name]);
 
   return (
-    <>
-      <h1>{name}</h1>
-      <h2>{types[0]}</h2>
-    </>
+    <div>
+      {pokemonData.map((data) => {
+        return (
+          <div className="container">
+            <img src={data.sprites["front_default"]} alt="pokemon" />
+            <div className="divTable">
+              <div className="divTableBody">
+                <h1>{name}</h1>
+
+                <div className="divTableRow">
+                  <div className="divTableCell">Type(s):</div>
+                  <div className="divTableCell">{pokemonTypes}</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Height:</div>
+                  <div className="divTableCell"> {data.height / 10} m</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Weight:</div>
+                  <div className="divTableCell"> {data.weight / 10} kg</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Number of Battles</div>
+                  <div className="divTableCell">{data.game_indices.length}</div>
+                </div>
+                <Link to="/">
+                  <button>Back</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
@@ -77,7 +114,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/pokemon/:name" element={<Pokemon />} />
       </Routes>
     </>
